148bf2eadeac9209d0baed2d696520f99abaff06
이정환/기술지원실/II <cookiegon3434@injeinc.co.kr>
2025-02-27 15:55:42 +0900

    Refactor LazyPagingGrid, SearchBar, and SearchViewModel to remove unused imports and improve code clarity

--- app/src/main/java/jeonghwan/app/favorite/ui/screen/search/ContentPagingSource.kt
+++ app/src/main/java/jeonghwan/app/favorite/ui/screen/search/ContentPagingSource.kt
@@ -1,6 +1,5 @@
 package jeonghwan.app.favorite.ui.screen.search
 
-import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import jeonghwan.app.favorite.domain.model.ContentEntity
@@ -8,13 +7,13 @@ import jeonghwan.app.favorite.domain.model.ContentQueryEntity
 import jeonghwan.app.favorite.domain.model.QuerySort
 import jeonghwan.app.favorite.domain.usecase.ContentUseCaseInterface
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
+import timber.log.Timber
 
 data class ContentPagingKey(
     val query: String,
     val imagePage: Int,
     val moviePage: Int,
-    val sort: QuerySort,
+    val sort: QuerySort = QuerySort.ACCURACY,
     val size: Int,
     val isImageEnd: Boolean = false,
     val isMovieEnd: Boolean = false
@@ -22,36 +21,44 @@ data class ContentPagingKey(
 
 class ContentPagingSource(
     private val contentUseCase: ContentUseCaseInterface,
-    private val initialKey: ContentPagingKey,
+    private val query: String
 ) : PagingSource<ContentPagingKey, ContentEntity>() {
 
     override suspend fun load(
         params: LoadParams<ContentPagingKey>
     ): LoadResult<ContentPagingKey, ContentEntity> = coroutineScope {
-        val key = params.key ?: initialKey
+        val loadSize = params.loadSize
+        val currentPage = params.key ?: ContentPagingKey(
+            query = query,
+            size = loadSize,
+            imagePage = 1,
+            moviePage = 1,
+        )
         val query = ContentQueryEntity(
-            query = key.query,
-            sort = key.sort,
-            imagePage = key.imagePage,
-            moviePage = key.moviePage,
-            size = key.size,
-            isImageEnd = key.isImageEnd,
-            isMovieEnd = key.isMovieEnd
+            query = currentPage.query,
+            sort = currentPage.sort,
+            imagePage = currentPage.imagePage,
+            moviePage = currentPage.moviePage,
+            size = currentPage.size,
+            isImageEnd = currentPage.isImageEnd,
+            isMovieEnd = currentPage.isMovieEnd
         )
 
         return@coroutineScope try {
             val result = contentUseCase.getContent(query)
             result.fold(
                 onSuccess = { pagingResult ->
+                    Timber.d("호출 호출 !! : query = ${currentPage.query}, imagePage = ${currentPage.imagePage}, moviePage = ${currentPage.moviePage}")
+
                     val nextKey = if (pagingResult.data.isEmpty()) null else {
-                        key.copy(
-                            imagePage = if(pagingResult.isImageLastPage) key.imagePage else key.imagePage + 1,
-                            moviePage = if(pagingResult.isMovieLastPage) key.moviePage else key.moviePage + 1,
+                        currentPage.copy(
+                            imagePage = if(pagingResult.isImageLastPage) currentPage.imagePage else currentPage.imagePage + 1,
+                            moviePage = if(pagingResult.isMovieLastPage) currentPage.moviePage else currentPage.moviePage + 1,
                             isImageEnd = pagingResult.isImageLastPage,
                             isMovieEnd = pagingResult.isMovieLastPage)
                     }
-                    val prevKey = if (key.imagePage == 1 && key.moviePage == 1) null
-                        else key.copy(imagePage = key.imagePage - 1, moviePage = key.moviePage - 1)
+                    val prevKey = if (currentPage.imagePage == 1 && currentPage.moviePage == 1) null
+                        else currentPage.copy(imagePage = currentPage.imagePage - 1, moviePage = currentPage.moviePage - 1)
                     LoadResult.Page(
                         data = pagingResult.data,
                         prevKey = prevKey,
@@ -71,6 +78,7 @@ class ContentPagingSource(
     // getRefreshKey(): 새로고침할 때 사용될 키를 반환합니다.
     // 여기서는 현재 보이는 페이지의 앵커(anchor) position을 기준으로 계산합니다.
     override fun getRefreshKey(state: PagingState<ContentPagingKey, ContentEntity>): ContentPagingKey? {
+        Timber.d("getRefreshKey 호출")
         return state.anchorPosition?.let { anchorPos ->
             state.closestPageToPosition(anchorPos)?.prevKey?.let { key ->
                 key.copy(imagePage = key.imagePage + 1, moviePage = key.moviePage + 1)
